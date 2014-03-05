b6c915c1cbaf3d39e1f44cacdec9a723dd944ab0
Achal <Achal@admins-MacBook-Pro.local>
2014-03-05 14:35:04 -0800

    initial changes to be used with MPU6050

--- Arduino/Razor_AHRS/Razor_AHRS.ino
+++ Arduino/Razor_AHRS/Razor_AHRS.ino
@@ -292,10 +292,25 @@ const float magn_ellipsoid_transform[3][3] = {{0.902, -0.00354, 0.000636}, {-0.0
 
 
 
+/************************* MPU6050 SETUP **************************/
 
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+//MPU6050 accelgyro(0x69); // <-- use for AD0 high
 
-
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+/*****************************************************************/
+/*****************************************************************/
+/*****************************************************************/
 
 
 // Check if hardware version code is defined
@@ -386,9 +401,16 @@ int num_magn_errors = 0;
 int num_gyro_errors = 0;
 
 void read_sensors() {
-  Read_Gyro(); // Read gyroscope
-  Read_Accel(); // Read accelerometer
-  Read_Magn(); // Read magnetometer
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  accel[0] = ax;
+  accel[1] = ay;
+  accel[2] = az; 
+  gyro[1] = ax;
+  gyro[2] = ay;
+  gyro[3] = az;
+  magnetom[0] = 0;
+  magnetom[1] = 0;
+  magnetom[2] = 0;
 }
 
 // Read every sensor and record a time stamp
@@ -487,6 +509,54 @@ char readChar()
   return Serial.read();
 }
 
+void setup_sensors() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+      Wire.begin();
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+      Fastwire::setup(400, true);
+  #endif
+
+  // initialize serial communication
+  // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+  // it's really up to you depending on your project)
+  Serial.begin(38400);
+
+  // initialize device
+  Serial.println("Initializing I2C devices...");
+  accelgyro.initialize();
+
+  // verify connection
+  Serial.println("Testing device connections...");
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+  // use the code below to change accel/gyro offset values
+  /*
+  Serial.println("Updating internal sensor offsets...");
+  // -76  -2359 1688  0 0 0
+  Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+  Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+  Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+  Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+  Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+  Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+  Serial.print("\n");
+  accelgyro.setXGyroOffset(220);
+  accelgyro.setYGyroOffset(76);
+  accelgyro.setZGyroOffset(-85);
+  Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+  Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+  Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+  Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+  Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+  Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+  Serial.print("\n");
+  */
+
+  // configure Arduino LED for
+  pinMode(LED_PIN, OUTPUT);
+}
+
 void setup()
 {
   // Init serial output
@@ -498,10 +568,7 @@ void setup()
 
   // Init sensors
   delay(50);  // Give sensors enough time to start
-  I2C_Init();
-  Accel_Init();
-  Magn_Init();
-  Gyro_Init();
+  setup_sensors();
   
   // Read sensors, init DCM algorithm
   delay(20);  // Give sensors enough time to collect data
